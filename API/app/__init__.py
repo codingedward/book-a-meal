f272fb8cd7cbaee5a8bff28288f20c101ba44d25
Edward Njoroge <codingedward@gmail.com>
2018-04-26 00:18:37 +0300

    Add api manager

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask
 from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy 
@@ -6,16 +7,19 @@ from instance.config import app_config
 
 db = SQLAlchemy()
 
-
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    api_manager = APIManager(app, flask_sqlalchemy_db=db)
-
-    # The routes will go here
+    with app.app_context():
+        from app.models import Meal
+        manager = APIManager(app, flask_sqlalchemy_db=db)
+        manager.create_api(
+            Meal,
+            methods=['GET', 'POST', 'DELETE', 'PUT'],
+            url_prefix='/api/v1'
+        )
 
     return app
-
