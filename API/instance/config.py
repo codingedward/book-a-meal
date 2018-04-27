0507adb0a2b0454afaedbe83d4d9c1e089eb3b63
Edward Njoroge <codingedward@gmail.com>
2018-04-27 03:08:19 +0300

    Remove database configuration from config

--- API/instance/config.py
+++ API/instance/config.py
@@ -6,15 +6,6 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True    # prevent cross site request forgery
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # auth endpoint
-    JWT_AUTH_URL_RULE = '/api/v1/auth/login'
-    # use email instead of password
-    JWT_AUTH_USERNAME_KEY = 'email'
-    JWT_EXPIRATION_DELTA = timedelta(seconds=3600)
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 
 class ProductionConfig(Config):
@@ -30,8 +21,6 @@ class DevConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
-
 
 app_config = {
     'dev': DevConfig,
