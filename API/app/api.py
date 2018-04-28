4459196f6e7101ca79cf3408dfc338505e3fac65
Edward Njoroge <codingedward@gmail.com>
2018-04-29 00:40:11 +0300

    Add jwt to routes

--- API/app/api.py
+++ API/app/api.py
@@ -1,15 +1,20 @@
-from app.model import BAM
+from app.model import BAM, UserType
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_raw_jwt
+)
 from flask import Blueprint, jsonify, request
 
 
 api = Blueprint('api', __name__)
 bam = BAM()
+blacklist = set()
 
 """
 Authentication Routes
 
 """
-@api.route('/api/v1/auth/register', methods=['POST'])
+@api.route('/api/v1/auth/signup', methods=['POST'])
 def register():
     if not request.is_json: 
         return jsonify({ 'message': 'Request should be JSON' }), 400
@@ -18,51 +23,61 @@ def register():
     if fails:
         return jsonify({'errors': errors}), 400
     else:
+        request.json['role'] = UserType.CUSTOMER
         return jsonify(bam.post_user(request.json)), 201
 
 @api.route('/api/v1/auth/login', methods=['POST'])
 def login():
-    if not request.is_json:
-        return jsonify({ 'errors': ['Request should be JSON'] }), 400
+    if not request.is_json: 
+        return jsonify({ 'message': 'Request should be JSON' }), 400
 
-    fails, errors = bam.validate_user_fails(request.json)
-    if fails:
-        return jsonify({'errors': errors}), 400
-    else:
-        return jsonify({
-            'access_token':  '<token>',
-            'message' : 'User successfully registered'
-        }), 201
+    if not request.json.get('email'):
+        return jsonify({'errors': [ 'Email is required' ]}), 400
+    if not request.json.get('password'):
+        return jsonify({'errors': [ 'Password is required' ]}), 400
+
+    access_token = create_access_token(identity=request.json['email'])
+    return jsonify(access_token=access_token)
+
+@api.route('/api/v1/auth/logout', methods=['DELETE'])
+@jwt_required
+def logout():
+    jti = get_raw_jwt()['jti']
+    blacklist.add(jti)
+    return jsonify({'msg': 'Successfully logged out.'}), 200
 
 """
 Meals Routes
 
 """
-@api.route('/api/v1/meals', methods=['POST', 'GET' ])
+@api.route('/api/v1/meals', methods=['POST'])
+@jwt_required
 def meals():
-    if request.method == 'POST':
-        if not request.is_json:
-            return jsonify({ 
-                'errors': ['Request should be JSON'] }), 400
+    if not request.is_json:
+        return jsonify({ 
+            'errors': ['Request should be JSON'] }), 400
 
-        fails, errors = bam.validate_meal_fails(request.json)
-        if fails:
-            return jsonify({ 'errors': errors }), 400
-        else:
-            return jsonify(bam.post_meal(request.json)), 201
+    current_user = bam.get_user_by_email(get_jwt_identity())
+    if not current_user['role'] == UserType.CATERER:
+        return jsonify({ 
+            'errors': ['Unauthorized access to non-caterer'] }), 401
 
-    elif request.method == 'GET':
-        return jsonify({'num_results': len(bam.meals), 
-                        'objects': list(bam.get_meals().values())})
+    fails, errors = bam.validate_meal_fails(request.json)
+    if fails:
+        return jsonify({ 'errors': errors }), 400
+    else:
+        return jsonify(bam.post_meal(request.json)), 201
 
-@api.route('/api/v1/meals/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
-def meal(id):
-    if request.method == 'GET':
-        if not id in bam.meals.keys():
-            return jsonify({ 'errors': ['Not Found'] }), 404
-        return jsonify(bam.get_meal(id)), 200
+@api.route('/api/v1/meals', methods=['GET'])
+def meals_get():
+    return jsonify({'num_results': len(bam.meals), 
+                    'objects': list(bam.get_meals().values())})
 
-    elif request.method == 'DELETE': 
+
+@api.route('/api/v1/meals/<int:id>', methods=['DELETE', 'PATCH'])
+@jwt_required
+def meal_modify(id):
+    if request.method == 'DELETE': 
         if not id in bam.meals.keys():
             return jsonify({ 'errors': ['Not Found'] }), 404
         bam.delete_meal(id)
@@ -83,36 +98,42 @@ def meal(id):
         bam.put_meal(request.json, id)
         return jsonify({ 'message': 'Successfully updated' }), 200
 
+@api.route('/api/v1/meals/<int:id>', methods=['GET'])
+def meal(id):
+    if not id in bam.meals.keys():
+        return jsonify({ 'errors': ['Not Found'] }), 404
+    return jsonify(bam.get_meal(id)), 200
+
+
 """
 Menus Routes
 
 """
 
-@api.route('/api/v1/menus', methods=['POST', 'GET' ])
+@api.route('/api/v1/menus', methods=['POST'])
+@jwt_required
 def menus():
-    if request.method == 'POST':
-        if not request.is_json:
-            return jsonify({ 
-                'errors': ['Request should be JSON'] }), 400
+    if not request.is_json:
+        return jsonify({ 
+            'errors': ['Request should be JSON'] }), 400
 
-        fails, errors = bam.validate_menu_fails(request.json)
-        if fails:
-            return jsonify({ 'errors': errors }), 400
-        else:
-            return jsonify(bam.post_menu(request.json)), 201
+    fails, errors = bam.validate_menu_fails(request.json)
+    print(errors)
+    if fails:
+        return jsonify({ 'errors': errors }), 400
+    else:
+        return jsonify(bam.post_menu(request.json)), 201
 
-    elif request.method == 'GET':
-        return jsonify({'num_results': len(bam.menus), 
-                        'objects': list(bam.get_menus().values())})
 
-@api.route('/api/v1/menus/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
-def menu(id):
-    if request.method == 'GET':
-        if not id in bam.menus.keys():
-            return jsonify({ 'errors': ['Not Found'] }), 404
-        return jsonify(bam.get_menu(id)), 200
+@api.route('/api/v1/menus', methods=['GET'])
+def menus_get():
+    return jsonify({'num_results': len(bam.menus), 
+                    'objects': list(bam.get_menus().values())})
 
-    elif request.method == 'DELETE': 
+@api.route('/api/v1/menus/<int:id>', methods=['PATCH', 'DELETE'])
+@jwt_required
+def menu_modify(id):
+    if request.method == 'DELETE': 
         if not id in bam.menus.keys():
             return jsonify({ 'errors': ['Not Found'] }), 404
         bam.delete_menu(id)
@@ -133,12 +154,19 @@ def menu(id):
         bam.put_menu(request.json, id)
         return jsonify({ 'message': 'Successfully updated' }), 200
 
+@api.route('/api/v1/menus/<int:id>', methods=['GET'])
+def menu(id):
+    if not id in bam.menus.keys():
+        return jsonify({ 'errors': ['Not Found'] }), 404
+    return jsonify(bam.get_menu(id)), 200
+
+
 """
 Orders Routes
 
 """
-
 @api.route('/api/v1/orders', methods=['POST', 'GET' ])
+@jwt_required
 def orders():
     if request.method == 'POST':
         if not request.is_json:
@@ -156,6 +184,7 @@ def orders():
                         'objects': list(bam.get_orders().values())})
 
 @api.route('/api/v1/orders/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
+@jwt_required
 def order(id):
     if request.method == 'GET':
         if not id in bam.orders.keys():
@@ -183,7 +212,13 @@ def order(id):
         bam.put_order(request.json, id)
         return jsonify({ 'message': 'Successfully updated' }), 200
 
+"""
+Notifications Routes
+
+"""
+
 @api.route('/api/v1/notifications', methods=['POST', 'GET' ])
+@jwt_required
 def notifications():
     if request.method == 'POST':
         if not request.is_json:
@@ -199,12 +234,9 @@ def notifications():
     elif request.method == 'GET':
         return jsonify({'num_results': len(bam.notifications), 
                         'objects': list(bam.get_notifications().values())})
-"""
-Notifications Routes
-
-"""
 
 @api.route('/api/v1/notifications/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
+@jwt_required
 def notification(id):
     if request.method == 'GET':
         if not id in bam.notifications.keys():
