77ee9c1ec7be41bf7db19d72f1abd3d05191c906
Edward Njoroge <codingedward@gmail.com>
2018-04-29 03:42:58 +0300

    Add DotEnv flask package

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,16 +1,18 @@
 import json
 from flask import Flask
 from collections import namedtuple
-from instance.config import app_config
 from flask_jwt_extended import (
     JWTManager, jwt_optional, create_access_token,
     get_jwt_identity
 )
 from app.api import api, bam, blacklist
+from instance.config import app_config
+from flask.ext.dotenv import DotEnv
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    env = DotEnv(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.register_blueprint(api)
