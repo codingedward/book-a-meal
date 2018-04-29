37c391cd296df2372c8d0728a83f8c8e378dc4a3
Edward Njoroge <codingedward@gmail.com>
2018-04-29 04:50:46 +0300

    Setup Travis CI

--- API/travis-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-export FLASK_APP="run.py"
-export APP_MODE='testing'
-export SECRET='1aodfadfjo23erofoasdfuoaisdufadadfuo32'
-export DEFAULT_ADMIN_USERNAME='admin'
-export DEFAULT_ADMIN_PASSWORD='super-secret'
-export DEFAULT_ADMIN_EMAIL='bam@admin.com'
-export JWT_SECRET_KEY='aoaudfouaeoru23432oiuriuiaudsfiadf'
