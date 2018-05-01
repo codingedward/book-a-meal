bd53e06cfaaf4c7646bb081f957ddee582df914f
Edward Njoroge <codingedward@gmail.com>
2018-05-01 15:00:59 +0300

    Use jwt-extended instead of jwt

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,32 +1,17 @@
 import sys
+from flask import Flask, Blueprint
 from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy 
-from flask import Flask, jsonify, request
-from flask_jwt import JWT, jwt_required, current_identity
-
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_raw_jwt
+)
 from instance.config import app_config
-
 
 db = SQLAlchemy()
 
-@jwt_required()
-def customer_auth(**kwargs):
-    """ 
-    We use this empty function to as a preprocessor in JWT restless
-    in order to include the @jwt_required decorator. That way, we
-    can gaurd our API endpoints.
-    """
-    pass
-
-@jwt_required()
-def caterer_auth(**kwargs):
-    """ 
-    This is similar to auth_func with the extra requirement of a user 
-    being a caterer
-    """
-    if not current_identity.is_caterer():
-        raise ProcessingException(detail='This user is not a caterer', 
-                                  status=400)
+from app.auth import auth, caterer_auth, customer_auth
+from app.models import Meal, User, Notification, Menu, Order
 
 
 def create_app(config_name):
@@ -34,51 +19,17 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    app.register_blueprint(auth)
 
-    from app.models import Meal, User, Notification, Menu, Order
-    """
-    requires to be imported under create_app to avoid circular imports
-    """
+    jwt = JWTManager(app)
 
-    def authenticate(email, password):
-        """ this gets called by the JWT scheme to authenticate user """
-        user = User.query.filter_by(email=email).first()
-        if user and user.validate_password(password):
-            return user
-
-    def identity(payload):
-        """ given a payload return user """
-        return User.query.get(payload['identity'])
+    @jwt.token_in_blacklist_loader
+    def check_token_in_blacklist(decrypted_token):
+        return False
 
     with app.app_context():
         """ we need to have our api routes while the app is running """
 
-        @app.route('/api/v1/auth/register', methods=['POST'])
-        def register():
-            """ 
-            This is the only endpoint we need to create ourselves as all the others
-            are provided by flask-restless
-
-            Authentication is handled by flask-jwt
-            """
-            if not request.is_json: 
-                return jsonify({'message': 'Request should be JSON'}), 400
-            username = request.json.get('username')
-            if not username:
-                return jsonify({'message': 'Name is required'}), 400
-            email = request.json.get('email')
-            if not email:
-                return jsonify({'message': 'Email is required'}), 400
-            password = request.json.get('password')
-            if not password:
-                return jsonify({'message': 'Password is required'}), 400
-
-            user = User(username=username, email=email, password=password)
-            user.save()
-            return jsonify({'message': 'User successfully registered'}), 201
-            
-
-        jwt = JWT(app, authenticate, identity)
         manager = APIManager(app, flask_sqlalchemy_db=db)
 
         manager.create_api(
@@ -134,4 +85,5 @@ def create_app(config_name):
                 'DELETE_MANY': [caterer_auth],
             }
         )
+
     return app
