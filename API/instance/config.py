7e821a16f3b6020054e8c12463dbe1403ab876bb
Edward Njoroge <codingedward@gmail.com>
2018-05-03 00:25:11 +0300

    Add JWT configurations

--- API/instance/config.py
+++ API/instance/config.py
@@ -6,11 +6,12 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True    # prevent cross site request forgery
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # auth endpoint
     JWT_AUTH_URL_RULE = '/api/v1/auth/login'
+
     # use email instead of password
     JWT_AUTH_USERNAME_KEY = 'email'
     JWT_EXPIRATION_DELTA = timedelta(seconds=3600)
@@ -30,7 +31,7 @@ class DevConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
 
 
 app_config = {
