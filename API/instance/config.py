f239f4b37f02ab35276c0c661fe9315bca643ad5
Edward Njoroge <codingedward@gmail.com>
2018-05-03 09:54:29 +0300

    Remove unnecessary config variables

--- API/instance/config.py
+++ API/instance/config.py
@@ -9,11 +9,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # auth endpoint
-    JWT_AUTH_URL_RULE = '/api/v1/auth/login'
-
-    # use email instead of password
-    JWT_AUTH_USERNAME_KEY = 'email'
     JWT_EXPIRATION_DELTA = timedelta(seconds=3600)
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
