96ee427b21eb95ff528da3cc1785bcf177332a8d
Edward Njoroge <codingedward@gmail.com>
2018-05-03 22:47:19 +0300

    Change to DATABASE_URL for Heroku

--- API/instance/config.py
+++ API/instance/config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True    # prevent cross site request forgery
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     JWT_EXPIRATION_DELTA = timedelta(seconds=3600)
@@ -26,7 +26,7 @@ class DevConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
 
 
 app_config = {
