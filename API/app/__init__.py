227ececc0e9925d7ec448c4e9051ecae2228fe8e
Edward Njoroge <codingedward@gmail.com>
2018-05-06 21:59:17 +0300

    Add request processors to handle orders

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -2,29 +2,23 @@ import sys
 from flask import (
     Flask, Blueprint, jsonify, send_from_directory, abort, make_response
 )
-from flask_restless import APIManager, ProcessingException
+from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy 
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity, get_raw_jwt
-)
+from flask_jwt_extended import JWTManager
 from instance.config import app_config
-from werkzeug.exceptions import HTTPException, default_exceptions
 
 
 db = SQLAlchemy()
 
 
 # these imports require the db
-from app.validators import (
-    validate_post_meal, validate_put_meal, validate_menu,
-    validate_notification, validate_user, 
-    validate_post_menu_item,  validate_post_order, validate_put_order,
-    validate_put_menu_item, AuthorizationError
-)
-from app.error_handlers import post_get, post_delete
-from app.auth import auth, caterer_auth, customer_auth
+from app.validators import Valid, AuthorizationError
+from app.error_handlers import errors_handler, blacklist_handler
+from app.auth import auth, caterer_auth, default_auth
 from app.models import Meal, User, Notification, Menu, Order, MenuItem
+from app.customize_routes import (
+    single_for_user, many_for_user, todays, post_delete, check_exists
+)
 
 
 def create_app(config_name):
@@ -33,22 +27,9 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
     app.register_blueprint(auth)
+    errors_handler(app)
     jwt = JWTManager(app)
-
-    @jwt.token_in_blacklist_loader
-    def check_token_in_blacklist(decrypted_token):
-        return Blacklist.query.filter_by(
-                    token=get_raw_jwt()['jti']).first() is not None
-
-    # jsonify http errors
-    for code in default_exceptions.keys():
-        @app.errorhandler(code)
-        def handle_error(ex):
-            return jsonify({'message': str(ex)}), code
-
-    @app.errorhandler(AuthorizationError)
-    def handle_authorization_error(err):
-        return jsonify({'message': str(err)}), 401
+    blacklist_handler(jwt)
 
     @app.route('/')
     def docs():
@@ -63,17 +44,17 @@ def create_app(config_name):
             methods=['GET', 'POST', 'DELETE', 'PUT'],
             url_prefix='/api/v1',
             preprocessors={
-                'POST': [caterer_auth, validate_post_meal],
-                'GET_MANY': [caterer_auth],
-                'GET_SINGLE': [caterer_auth],
-                'PUT_SINGLE': [caterer_auth, validate_put_meal],
+                'POST': [caterer_auth, Valid.post_meal],
+                'GET_MANY': [default_auth],
+                'GET_SINGLE': [default_auth, check_exists(Meal)],
+                'PUT_SINGLE': [caterer_auth, Valid.put_meal,
+                               check_exists(Meal)],
                 'DELETE_SINGLE': [caterer_auth],
                 'DELETE_MANY': [caterer_auth],
             },
             postprocessors={
                 'DELETE_SINGLE': [post_delete],
                 'DELETE_MANY': [post_delete],
-                'GET_SINGLE': [post_get]
             }
         )
 
@@ -83,10 +64,11 @@ def create_app(config_name):
             url_prefix='/api/v1',
             collection_name='menu',
             preprocessors={
-                'POST': [caterer_auth, validate_menu],
-                'GET_SINGLE': [customer_auth],
-                'GET_MANY': [customer_auth],
-                'PUT_SINGLE': [caterer_auth, validate_menu],
+                'POST': [caterer_auth, Valid.menu],
+                'GET_SINGLE': [default_auth, check_exists(Menu)],
+                'GET_MANY': [default_auth, todays],
+                'PUT_SINGLE': [caterer_auth, Valid.menu,
+                               check_exists(Menu)],
                 'DELETE_SINGLE': [caterer_auth],
                 'DELETE_MANY': [caterer_auth],
             },
@@ -101,10 +83,11 @@ def create_app(config_name):
             methods=['GET', 'POST', 'DELETE', 'PUT'],
             url_prefix='/api/v1',
             preprocessors={
-                'POST': [caterer_auth, validate_post_menu_item],
-                'GET_SINGLE': [customer_auth],
-                'GET_MANY': [customer_auth],
-                'PUT_SINGLE': [caterer_auth, validate_put_menu_item],
+                'POST': [caterer_auth, Valid.post_menu_item],
+                'GET_SINGLE': [default_auth, check_exists(MenuItem)],
+                'GET_MANY': [default_auth],
+                'PUT_SINGLE': [caterer_auth, Valid.put_menu_item,
+                               check_exists(MenuItem)],
                 'DELETE_SINGLE': [caterer_auth],
                 'DELETE_MANY': [caterer_auth],
             },
@@ -119,11 +102,13 @@ def create_app(config_name):
             methods=['GET', 'POST', 'DELETE', 'PUT'],
             url_prefix='/api/v1',
             preprocessors={
-                'POST': [customer_auth, validate_post_order],
-                'GET_SINGLE': [customer_auth],
-                'GET_MANY': [caterer_auth],
-                'PUT_SINGLE': [customer_auth, validate_put_order],
-                'DELETE_SINGLE': [customer_auth],
+                'POST': [default_auth, Valid.post_order],
+                'GET_SINGLE': [default_auth, check_exists(Order), 
+                               single_for_user(Order)],
+                'GET_MANY': [default_auth, many_for_user],
+                'PUT_SINGLE': [default_auth, Valid.put_order,
+                               check_exists(Order)],
+                'DELETE_SINGLE': [default_auth],
                 'DELETE_MANY': [caterer_auth],
             },
             postprocessors={
@@ -137,12 +122,13 @@ def create_app(config_name):
             methods=['GET', 'POST', 'DELETE', 'PUT'],
             url_prefix='/api/v1',
             preprocessors={
-                'POST': [caterer_auth, validate_notification],
-                'GET_SINGLE': [customer_auth],
-                'GET_MANY': [customer_auth],
-                'PUT_SINGLE': [caterer_auth, validate_notification],
-                'DELETE_SINGLE': [customer_auth],
-                'DELETE_MANY': [customer_auth],
+                'POST': [caterer_auth, Valid.notification],
+                'GET_SINGLE': [default_auth, check_exists(Notification)],
+                'GET_MANY': [default_auth],
+                'PUT_SINGLE': [caterer_auth, Valid.notification,
+                               check_exists(Notification)],
+                'DELETE_SINGLE': [default_auth],
+                'DELETE_MANY': [default_auth],
             },
             postprocessors={
                 'DELETE_SINGLE': [post_delete],
