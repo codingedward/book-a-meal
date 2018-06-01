a5e27cae3533226998ffabdb339e4d01ddb8b62d
Edward Njoroge <codingedward@gmail.com>
2018-06-01 22:10:29 +0300

    Move exceptions handler [Fix #158031953]

---
+++ API/app/exceptions/handler.py
@@ -0,0 +1,20 @@
+from flask import abort, make_response, jsonify, Blueprint
+from app.models import Blacklist
+from flask_jwt_extended import get_raw_jwt
+from werkzeug.exceptions import HTTPException, default_exceptions
+from app.validators import AuthorizationError
+
+
+def init_app(app):
+    # jsonify http errors
+    for code in default_exceptions.keys():
+        @app.errorhandler(code)
+        def handle_error(ex):
+            return jsonify({'message': str(ex)}), code
+
+
+def init_jwt(jwt):
+    @jwt.token_in_blacklist_loader
+    def check_token_in_blacklist(decrypted_token):
+        return Blacklist.query.filter_by(
+                    token=decrypted_token['jti']).first() is not None
