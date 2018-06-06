10ad2db611027ee5c250213e8a511a13ee5b0b78
Edward Njoroge <codingedward@gmail.com>
2018-06-06 12:06:11 +0300

    Update order to support meal items quantitys

--- API/app/middlewares/order_middlewares.py
+++ API/app/middlewares/order_middlewares.py
@@ -2,12 +2,20 @@
 
 
 from app.validators import Valid
-from app.models import Order
+from app.models import Order, MenuItem
 from .common_middlewares import (
     single_for_user, many_for_user, post_delete, check_exists
 )
 from .auth_middlewares import caterer_auth, default_auth
 
+
+def update_quantity(result=None, **kwargs):
+    """When an order is made, we need to decrement the number
+    of available menu items"""
+    if result:
+        menu_item = MenuItem.query.get(result['menu_item_id'])
+        menu_item.quantity -= result['quantity']
+        menu_item.save()
 
 pre_order={
     'POST': [default_auth, Valid.post_order],
@@ -19,6 +27,8 @@ pre_order={
 }
 
 post_order={
+    'POST': [update_quantity],
+    'PUT_SINGLE': [update_quantity],
     'DELETE_SINGLE': [post_delete],
     'DELETE_MANY': [post_delete]
 }
